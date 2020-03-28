e120fc9aba9222e9a962052983f70ea10ef148b9
Lívia Jesenská <livia.jesenska@student.tuke.sk>
2020-03-28 21:20:23 +0100

    fixed fontsizes

--- build/main.py
+++ build/main.py
@@ -6,19 +6,21 @@ from kivy.uix.label import Label
 
 class MainApp(App):
     def build(self):
+        fontSize = 80
+        numberFontSize = 120
         main_layout = BoxLayout(orientation="vertical", padding=[50, 5])
         lengthLayout = BoxLayout(orientation="horizontal", padding = [0, 5], spacing = 20)
-        lengthLabel = Label(text="Dĺžka (m)", font_size=20, halign="left")
-        self.length = TextInput(text="0", multiline=False, readonly=False, halign="right", font_size=40)
+        lengthLabel = Label(text="Dĺžka (m)", font_size=fontSize, halign="left")
+        self.length = TextInput(text="0", multiline=False, readonly=False, halign="right", font_size=numberFontSize)
         dimensionLayout = BoxLayout(orientation="horizontal", padding = [0, 5], spacing = 20)
-        dimensionsLabel = Label(text="Výška etikety (mm)", font_size=20, halign="left")
-        self.dimensions = TextInput(text="0", multiline=False, readonly=False, halign="right", font_size=40)
+        dimensionsLabel = Label(text="Výška (mm)", font_size=fontSize, halign="left")
+        self.dimensions = TextInput(text="0", multiline=False, readonly=False, halign="right", font_size=numberFontSize)
         quantityLayout = BoxLayout(orientation="horizontal", padding = [0, 5], spacing = 20)
-        quantityLabel = Label(text="Počet etikiet", font_size=20, halign="left")
-        self.quantity = TextInput(text="0", multiline=False, readonly=False, halign="right", font_size=40)
+        quantityLabel = Label(text="Počet etikiet", font_size=fontSize, halign="left")
+        self.quantity = TextInput(text="0", multiline=False, readonly=False, halign="right", font_size=numberFontSize)
         productionLayout = BoxLayout(orientation="horizontal", padding = [0, 5], spacing = 20)
-        productionLabel = Label(text="Produkcia", font_size=20, halign="left")
-        self.production = TextInput(text="1", multiline=False, readonly=False, halign="right", font_size=40)
+        productionLabel = Label(text="Produkcia", font_size=fontSize, halign="left")
+        self.production = TextInput(text="1", multiline=False, readonly=False, halign="right", font_size=numberFontSize)
         lengthLayout.add_widget(lengthLabel)
         lengthLayout.add_widget(self.length)
         main_layout.add_widget(lengthLayout)
@@ -32,10 +34,10 @@ class MainApp(App):
         productionLayout.add_widget(self.production)
         main_layout.add_widget(productionLayout)
         buttonBox = BoxLayout(orientation="horizontal", padding = [0, 5], spacing = 20)
-        equalsButton = Button(text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}, font_size=40)
+        equalsButton = Button(text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}, font_size=fontSize)
         equalsButton.bind(on_press=self.onButtonClick)
         buttonBox.add_widget(equalsButton)
-        zeroButton = Button(text="C", pos_hint={"center_x": 0.5, "center_y": 0.5}, font_size=40)
+        zeroButton = Button(text="C", pos_hint={"center_x": 0.5, "center_y": 0.5}, font_size=fontSize)
         zeroButton.bind(on_press=self.zeroValues)
         buttonBox.add_widget(zeroButton)
         main_layout.add_widget(buttonBox)
@@ -57,10 +59,10 @@ class MainApp(App):
             self.dimensions.text = result*1000
     
     def zeroValues(self, instance):
-        self.length.text = "0";
-        self.dimensions.text = "0";
-        self.quantity.text = "0";
-        self.production.text = "1";
+        self.length.text = "0"
+        self.dimensions.text = "0"
+        self.quantity.text = "0"
+        self.production.text = "1"
 
 if __name__ == "__main__":
     app = MainApp()
